1e4c07ec1afe14b9de19603e625ef69c05f3e01f
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-12-08 16:57:27 -0600

    Add scroll functionality to SimilarProducts component

--- src/components/Store/Store/SimilarProducts.jsx
+++ src/components/Store/Store/SimilarProducts.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { useMediaQuery } from 'react-responsive';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { useRouter } from 'next/router';
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 function SimilarProducts({similarProducts}) {
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
@@ -27,18 +28,69 @@ function SimilarProducts({similarProducts}) {
 
     return stars;
   };
+ const { ref: inViewRef, inView } = useInView({
+    triggerOnce: true,
+  });
 
-   const { ref, inView } = useInView({
-          triggerOnce: true,
-        });
+  const scrollContainerRef = useRef(null);
 
+  const [isAtLeft, setIsAtLeft] = useState(true);
+  const [isAtRight, setIsAtRight] = useState(false);
+
+  useEffect(() => {
+    const checkScrollPosition = () => {
+      setIsAtLeft(scrollContainerRef.current.scrollLeft === 0);
+      setIsAtRight(
+        scrollContainerRef.current.scrollLeft +
+          scrollContainerRef.current.offsetWidth >=
+          scrollContainerRef.current.scrollWidth - 1
+      );
+    };
+
+    checkScrollPosition();
+    scrollContainerRef.current.addEventListener("scroll", checkScrollPosition);
+
+    return () => {
+      if (scrollContainerRef.current) {
+        scrollContainerRef.current.removeEventListener(
+          "scroll",
+          checkScrollPosition
+        );
+      }
+    };
+  }, []);
+
+  const rightDesplacement = () => {
+    scrollContainerRef.current.scrollLeft += 200;
+  };
+
+  const leftDesplacement = () => {
+    scrollContainerRef.current.scrollLeft -= 200;
+  };
 
   return (
-    <div className="w-full h-full flex flex-row justify-start gap-8 mt-8 overflow-x-scroll scrollbar-hide
-    px-4
-    "
-    ref={ref}
-    >
+    <div className="w-full h-full relative" ref={inViewRef}>
+    {!isAtLeft && (
+      <div
+      className="w-[70px] h-[70px] bg-red-500 rounded-full absolute top-[150px] left-0 z-10 flex flex-col justify-center items-center cursor-pointer"
+      onClick={leftDesplacement}
+      >
+      <FaArrowLeft className="text-white text-[30px]" />
+      </div>
+      )}
+      {!isAtRight && (
+        <div
+        className="w-[70px] h-[70px] bg-red-500 rounded-full absolute top-[150px] right-0 z-10 flex flex-col justify-center items-center cursor-pointer"
+        onClick={rightDesplacement}
+        >
+        <FaArrowRight className="text-white text-[30px]" />
+        </div>
+        )}
+        <div className="w-full h-full flex flex-row justify-start gap-8 mt-8 overflow-x-scroll scrollbar-hide
+        px-4
+        "
+        ref={scrollContainerRef}
+        >
       {similarProducts.map((product, index) => {
        
         return (
@@ -75,6 +127,7 @@ function SimilarProducts({similarProducts}) {
           </motion.div>
         );
       })}
+    </div>
     </div>
   );
 }
